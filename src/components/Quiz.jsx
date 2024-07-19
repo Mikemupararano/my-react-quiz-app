dce18583f0f5c5ef9eadd06e82655f439f1f3a32
Mikemupararano <kudath@yahoo.co.uk>
2024-07-19 18:04:20 +0100

    Wrote code for displaying question number and question total.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { useState } from 'react';
 const Quiz = ({ questions }) => {
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const { question, choices, correctAnswer } = questions[currentQuestion];
     return (
         <div className='quiz-container'>
-            <h1>Quiz</h1>
+            <>
+                <span className="active-question-no">{currentQuestion + 1}</span>
+                <span className="total-questions">/{questions.length }</span>
+            </>
+            
            
           
         </div>
