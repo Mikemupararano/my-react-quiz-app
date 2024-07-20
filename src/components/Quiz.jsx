f06bffb0c31737c8622e927c53ad805cf74e64f3
Mikemupararano <kudath@yahoo.co.uk>
2024-07-20 16:17:15 +0100

    Added visual representation of selected answers.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+
 const Quiz = ({ questions }) => {
     const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [selectedAnswerIdx, setSelectedAnswerIdx] = useState(null);
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
+
     const { question, choices, correctAnswer } = questions[currentQuestion];
+
+    const onAnswerClick = (answer, index) => {
+        setSelectedAnswerIdx(index);
+        setIsAnswerCorrect(answer === correctAnswer);
+    };
+
     return (
         <div className='quiz-container'>
-            <>
+            <div className='question-info'>
                 <span className="active-question-no">{currentQuestion + 1}</span>
-                <span className="total-questions">/{questions.length }</span>
-            </>
-            
-           
-          
+                <span className="total-question">/{questions.length}</span>
+            </div>
+            <h2>{question}</h2>
+            <ul className='choices-list'>
+                {choices.map((choice, index) => (
+                    <li
+                        onClick={() => onAnswerClick(choice, index)}
+                        key={choice}
+                        className={selectedAnswerIdx === index ? 'selected-answer' : ''}
+                    >
+                        {choice}
+                    </li>
+                ))}
+            </ul>
+            {isAnswerCorrect !== null && (
+                <div className={`answer-feedback ${isAnswerCorrect ? 'correct' : 'incorrect'}`}>
+                    {isAnswerCorrect ? 'Correct!' : 'Incorrect!'}
+                </div>
+            )}
         </div>
     );
 };
